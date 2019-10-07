7bb6e9fe8bb3fb8147387d91e2bf1d56f869e661
fredchenbj <cfworking@163.com>
2019-10-07 17:50:14 +0800

    refactor sth.
    Signed-off-by: fredchenbj <cfworking@163.com>

--- presplit.sh
+++ presplit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-## ./presplit.sh 10.136.16.2:2379 test 3
+## ./resources/bin/tikv-presplit --pd $PD --table-id "6D657461" --shard-bits 0  // meta
 if [ $# -lt 1 ];
 then
-	>&2 echo "usage $0 <pd> <table-name> <shard-bits(0~7)> [username]"
+	>&2 echo "usage $0 <pd> <table-id> <shard-bits(0~7)> [username]"
 	exit 1
 fi
 
 PD=$1
-TABLENAME=$2
+TABLEID=$2
 
 if [ "$3" -gt 7 ] || [ "$3" -lt 0 ];
 then 
@@ -22,6 +22,6 @@ then
 	USERNAME=$4
 fi
 
-## ./bin/proxy-ctl -pdaddr 10.136.16.1:2379 createTable "test" 4 "cf"
-## ./bin/tikv-bulkload --pd 10.136.16.1:2379 --table-name "test" --shard-bits 4
-./resources/bin/proxy-ctl -pdaddr $PD createTable $TABLENAME $SHARDBITS $USERNAME && ./resources/bin/tikv-presplit --pd $PD --table-name $TABLENAME --shard-bits $SHARDBITS
+## ./resources/bin/proxy-ctl -pdaddr $PD createTable "test" 4 "cf"
+## ./resources/bin/tikv-presplit --pd $PD --table-id "test" --shard-bits 4
+./resources/bin/proxy-ctl -pdaddr $PD createTable $TABLEID $SHARDBITS $USERNAME && ./resources/bin/tikv-presplit --pd $PD --table-id $TABLEID --shard-bits $SHARDBITS
